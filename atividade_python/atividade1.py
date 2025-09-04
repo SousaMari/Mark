e3987609b531fc45ac171feac8c42eb24dff09c9
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-04 00:31:47 -0300

    Questão 01

---
+++ atividade_python/atividade1.py
@@ -0,0 +1,10 @@
+numero = input('Digite um número de 1  a 3: ')
+
+if numero == "1":
+    print("um")
+elif numero == "2":
+    print("dois")
+elif numero == "3":
+    print("três")
+else:
+    print("Número inválido")    
