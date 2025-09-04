bc995606a4c325e493c81908121e510af43882f1
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-04 16:46:10 -0300

    Questão 18

---
+++ atividade_python/atividade18.py
@@ -0,0 +1,8 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+if num1 > 0 and num2 > 0 and num3 > 0:
+    print("Todos os números são positivos.")
+else:
+    print("Nem todos os números são positivos.")
