0254cbeced80081f79486b4f20fff9670e71eeaf
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-04 16:22:08 -0300

    Questão 14

---
+++ atividade_python/atividade14.py
@@ -0,0 +1,9 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+
+if soma > 100:
+    print("A soma é maior que 100.")
+else:
+    print("A soma não é maior que 100.")
