5252000baae1b705422e8f9dc63eac4cffc7d7a3
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-08 14:10:19 -0300

    Quetão 26

---
+++ atividade_python/atividade26.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+if numero1 % 5 == 0 and numero2 % 5 == 0:
+    print("Ambos os números são múltiplos de 5.")
+else:
+    print("Pelo menos um dos números não é múltiplo de 5.")    
+              
