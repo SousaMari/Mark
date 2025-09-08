d0d01395db4b45c0ed12a608dc6f7b22c6240ad8
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-08 16:21:44 -0300

    Questão 35

---
+++ atividade_python/atividade35.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+           
+if (num1 * num2) == 20:
+    print("A multiplicação dos números é igual a 20.")
+else:
+    print("A multiplicação dos números NÃO é igual a 20.")                                            
