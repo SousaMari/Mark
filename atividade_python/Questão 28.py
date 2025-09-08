966b745d0af2eb659e5fba2d64a802e6ebf8a179
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-08 14:53:43 -0300

    Questão 28

---
+++ atividade_python/Questão 28.py
@@ -0,0 +1,7 @@
+palavra = input("Digite uma palavra: ")
+tamanho = len(palavra)
+
+if tamanho > 5:
+    print(f"A palavra '{palavra}' tem mais de 5 letras. ")
+else:
+    print(f"A palavra '{palavra}' não tem mais de 5 letras.")   
