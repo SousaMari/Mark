fb855e0be70b116e1a8f879400e8a62b2a64d788
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-08 13:02:08 -0300

    Questão  24

---
+++ atividade_python/atividade24.py
@@ -0,0 +1,7 @@
+cidade = input("Digite o nome de uma cidade: ")
+cidade_minuscula = cidade.lower()
+
+if cidade_minuscula == "brasília":
+    print("Brasília é a capital do Brasil. ")
+else:
+    print("Essa cidade não é a capital do Brasil. ")    
---
+++ atividade_python/atividade25.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número de 0 a 20: "))
+             
+if numero >= 10 and numero <=15:
+    print("O número está entre 10 e 15.")
+else:
+    print("O número NÃO está entre 10 e 15.")    
+
