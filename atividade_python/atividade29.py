81bf7cbc65bd09560da14d7deaf2b22dac9a5779
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-08 15:01:04 -0300

    Questão 29

---
+++ atividade_python/atividade29.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número: "))
+
+if numero % 3 == 0:
+    print(f"O número '{numero}' é multiplo de 3. ")
+else:
+    print(f"O número '{numero} não é multiplo de 3. ")
+        
