cc7d78adef4bb0b3e187d36d43484edb4b8b122e
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-09 15:00:35 -0300

    Questão 38

---
+++ atividade_python/atividade38.py
@@ -0,0 +1,6 @@
+altura = float(input("Digite sua altura em metros: "))
+
+if altura > 1.75:
+    print (f" Sua altura é {altura}m é maior que 1.75m.")
+else:
+    print (f"Sua altura é {altura}m é menor que 1.75m.")    
