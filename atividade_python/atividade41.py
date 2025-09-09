a77fbc5e56363e819f95979ee5f9101f481743e0
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-09 15:42:07 -0300

    Questão  41

---
+++ atividade_python/atividade41.py
@@ -0,0 +1,9 @@
+numero_final = int(input("Digite um número inteiro positivo: "))
+
+
+if numero_final > 0:
+    for numero in range(1, numero_final + 1):
+        print(numero)
+else:
+    print("Por favor, digite um número interiro positivo.")
+            
