62c074d1813b7d3307eb178cb1f6d74eb19086e0
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-09 15:16:48 -0300

    Questão 40

---
+++ atividade_python/atividade40.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: ")) 
+num3 = int(input("Digite o terceiro número: ")) 
+
+if num1 == num2 and num2 == num3:
+    print("Os três números são iguais.")
+else:
+    print("Os números não são todos iguais.")   
