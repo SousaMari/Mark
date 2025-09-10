ffc77c47366c80c8ac9fcb799ab2ade63bb0a52a
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-10 13:56:59 -0300

    Questão 56

---
+++ atividade_python/atividade56.py
@@ -0,0 +1,6 @@
+quantidade = int(input("Quantas vezes você quer que a mensagem seja exibida? "))
+contador = 0
+
+while contador < quantidade:
+    print("Esta é a mensagem número", contador + 1)
+    contador += 1 
