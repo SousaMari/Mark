7646355380d7c2d62c7e919489d2bda4ebd66c69
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-10 13:39:54 -0300

    Questão 54

---
+++ atividade_python/atividade54.py
@@ -0,0 +1,47 @@
+while True:
+    numero = float(input("Digite um número (negativo para parar): "))
+
+    if numero < 0:
+        print("Número negativo digitado. Programa encerrado.")
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
