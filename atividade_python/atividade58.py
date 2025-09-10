d05b2b77499cf0d196859cf5635b056512ce9e8c
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-10 16:05:19 -0300

    Questão 58

---
+++ atividade_python/atividade58.py
@@ -0,0 +1,5 @@
+palavra = input("Digite uma palavra (ou 'sair' para encerrar): ").strip().lower()
+
+while palavra != "sair":
+    palavra = input("Digite uma palavra (ou 'sair' para encerrar): ").strip().lower()
+print("Programa encerrado.")    
