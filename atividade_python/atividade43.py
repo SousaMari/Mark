fbf51e7f4d1bb3946bf143fd213f3ddc114d4523
Marinaria Sousa <marinnaria25@gmail.com>
2025-09-09 22:02:45 -0300

    Questão 43

---
+++ atividade_python/atividade43.py
@@ -0,0 +1,8 @@
+quantidade_repeticoes = int(input("Quantas vezes você quer repetir a mensagem? "))
+
+mensagem = "Olá! Esta é a mensagem."
+
+for i in range(quantidade_repeticoes):
+    print(mensagem)
+
+print("\nFim do programa.")    
